04d512a8ff07fa19bdd08c9fed774a5fedca0797
Zain Ahmad <zainsattar17@gmail.com>
2024-01-23 15:50:12 +0500

    add a new car api

--- src/routes/car.js
+++ src/routes/car.js
@@ -1,4 +1,6 @@
 const api = require('express').Router();
+const multer = require('multer')
+const upload = multer()
 const auth=require("../middleware/auth")
 const database = require('../config/database')
 const { mySqlDb } = database
@@ -21,5 +23,53 @@ api.get('/models',auth, (req, res) => {
         res.status(500).json({ error: 'Unable to Find Car Model', message: error.message });
     }
 });
+
+api.post('/',upload.any(),(req,res)=>{
+    try{
+        // const { renter_id, company_id, car_name, color, fuel_type, mileage, engine, kms_driven, address, longitude, latitude, province, city, famous_place_nearby} = req.body
+        const {renter_id,company_id,car_name,province,city,address}=req.body
+        if(!renter_id){
+            res.status(500).json({error:"renter_id should not be empty"})
+            return
+        }
+        if(!company_id){
+            res.status(500).json({error:"company_id should not be empty"})
+            return
+        }
+        if(!car_name){
+            res.status(500).json({error:"car_name should not be empty"})
+            return
+        }
+        if(!address){
+            res.status(500).json({error:"address should not be empty"})
+            return
+        }
+        if(!city){
+            res.status(500).json({error:"city should not be empty"})
+            return
+        }
+        if(!province){
+            res.status(500).json({error:"province should not be empty"})
+            return
+        }
+        const files = req.files
+        const Image1 = files.find(file => file.fieldname === 'Image1')?.buffer
+        const Image2 = files.find(file => file.fieldname === 'Image2')?.buffer
+        const Image3 = files.find(file => file.fieldname === 'Image3')?.buffer
+        const Image4 = files.find(file => file.fieldname === 'Image4')?.buffer
+
+        const car={...req.body,Image1,Image2,Image3,Image4}
+        mySqlDb.query('INSERT INTO cars SET ?', car, (error, result) => {
+            if (error) {
+                res.status(500).json({ error: 'Car is not added.', message: error.message });
+            } else {
+              res.status(200).json({ message: "Car added successfully." });
+            }
+          })
+
+    }catch(error){
+        res.status(500).json({ error: 'Car is not added...', message: error.message });
+    }
+})
 
 module.exports = api;
