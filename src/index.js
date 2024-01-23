957ee5048c81ee5520a42db9fa3dbf67f592ad8d
Zain Ahmad <zainsattar17@gmail.com>
2024-01-23 14:38:56 +0500

    modified folder structure

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const express = require('express');
-const authApi = require('./routes/authApi');
+const userApi = require('./routes/user');
 const homeApi = require('./routes/home')
-const carModelsApi=require('./routes/carModel')
+const carApi = require('./routes/car')
 const database = require('./config/database');
 var bodyParser = require('body-parser')
 
@@ -18,9 +18,9 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 // app.use(express.json());
 
-app.use('/api/users', authApi);
+app.use('/api/users', userApi);
 app.use('/api/home', homeApi);
-app.use("/api/car",carModelsApi)
+app.use("/api/car",carApi)
 
 mySqlDb.connect((error) => {
   if (error) console.log(error)
