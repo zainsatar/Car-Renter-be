026126d8d3cc88bc0bee4dd4b5b80a321e348cde
Zain Ahmad <zainsattar17@gmail.com>
2024-01-24 13:45:18 +0500

    configured mySql pooling parameters

--- src/config/database.js
+++ src/config/database.js
@@ -5,19 +5,20 @@ const DB_USER=process.env.DB_USER
 const DB_USER_PASSWORD=process.env.DB_USER_PASSWORD
 const DATABASE=process.env.DATABASE
 
-const connectionItem = { host: DB_HOST, user: DB_USER, password: DB_USER_PASSWORD, database: DATABASE,waitForConnections: true, connectionLimit: 100, queueLimit: 0 }
+const connectionItem = {
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_USER_PASSWORD, 
+    database: DATABASE,
+    waitForConnections: true,
+    connectionLimit: 20,
+    maxIdle: 20, // max idle connections, the default value is the same as `connectionLimit`
+    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
+}
 
 const pool = mysql.createPool(connectionItem)
-// pool.getConnection()
-//   .then(conn => {
-//     const res = conn.query('SELECT 1');
-//     conn.release();
-//     return res;
-//   }).then(results => {
-//     console.log('Connected to MySQL DB');
-//   }).catch(err => {
-//     console.log(err); 
-//   });
-
   
 module.exports = pool
