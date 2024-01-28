c3ee13d87a590f4f0f68ddc74c870688ece0748c
Crazy-Coder8665 <talha8665i@gmail.com>
2024-01-28 21:16:31 +0500

    unauthorization error added

--- helper/authHelper.php
+++ helper/authHelper.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../helper/sendJson.php';
 
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
@@ -12,6 +13,9 @@ function validateToken($token)
     global $secretKey;
 
     try {
+        if (empty($token))
+            sendJson(401, 'Unauthorized');
+
         $decoded = JWT::decode($token, new Key($secretKey, 'HS256'));
         return $decoded;
     } catch (Exception $e) {
